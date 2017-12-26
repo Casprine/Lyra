4514957a709183de2b5acd3349d84534ec5f93f5
Casprine Assempah <casprine.001@gmail.com>
2017-12-26 16:46:35 +0000

    :gem: dashboard brought to life

--- dashboard/src/components/ContentForm.js
+++ dashboard/src/components/ContentForm.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
-import { Card, Form, Button } from 'semantic-ui-react';
+import { Card, Form, Button, TextArea, Dropdown, Grid} from 'semantic-ui-react';
 import axios from 'axios';
+
+
+const options = [
+    { key: 'css', text: 'CSS', value: 'css' },
+    { key: 'design', text: 'Graphic Design', value: 'design' },
+    { key: 'ember', text: 'Ember', value: 'ember' },
+    { key: 'html', text: 'HTML', value: 'html' },
+]
 
 
 class ContentForm extends Component{
@@ -23,6 +31,10 @@ class ContentForm extends Component{
         this.setState({ bodyText : value })
     }
 
+    onImageLinkChange(value){
+        this.setState({ ImageLink : value})
+    }
+
     onSubmitButtonClicked(){
         this.setState({ formLoading : true });
         axios.get('/hello')
@@ -37,30 +49,78 @@ class ContentForm extends Component{
 
     render(){
         return(
-            <div>
-                <Card>
+            <Grid columns={2} divided>
+                <Grid.Row>
+                    <Grid.Column>
+                        <Card raised="1">
+                            <Card.Content>
+                                <Card.Header>
+                                    Add New Resource
+                                </Card.Header>
+                                <Form loading={this.state.formLoading}>
+                                    <Form.Field>
+                                        <label className="center">Title</label>
+                                        <input name='titleText' value={this.state.titleText} onChange={event => this.onTitleTextChange(event.target.value)}/>
+                                    </Form.Field>
+
+                                    <Form.Field>
+                                        <label className="center">Body</label>
+                                        <TextArea placeholder='about the product' style={{ maxHeight: 100 }} />                    
+                                    </Form.Field>
+
+                                    <Form.Field>
+                                        <label className="center" >
+                                            Image -- Link
+                                        </label>
+                                        <input name="imageLink" value={this.state.ImageLink} onChange={event => this.onImageLinkChange(event.target.value)}/>
+                                    </Form.Field>
+
+                                    <Form.Field>
+                                        <label className="center" >
+                                            Resource -- Link
+                                        </label>
+                                        <input name="imageLink" value={this.state.ImageLink} onChange={event => this.onImageLinkChange(event.target.value)} />
+                                    </Form.Field>
+
+                                    <Form.Field>
+                                        <label className="center">
+                                        Tag
+                                        </label>
+                                    <Dropdown placeholder='tags' fluid multiple selection options={options} />        
+                                    </Form.Field>
+
+                                    <Button color="blue" onClick={() => this.onSubmitButtonClicked()} className="btn">
+                                        Submit
+                                </Button>
+
+                                </Form>
+                       </Card.Content>
+                    </Card>
+                </Grid.Column>
+
+
+
+
+
+            <Grid.Column>
+                <Card raised="1">
                     <Card.Content>
-                        <Card.Header>
-                            Add New Content
-                        </Card.Header>
-                        <Form loading={this.state.formLoading}>
+                        <Form>
                             <Form.Field>
-                                <label>Title</label>
-                                <input name='titleText' value={this.state.titleText} onChange={event => this.onTitleTextChange(event.target.value)}/>
+                                <label className="center">
+                                    Tag
+                                </label>
+                                <input name="keys"/>
                             </Form.Field>
+                            
 
-                            <Form.Field>
-                                <label>Body</label>
-                                <input name='bodyText' value={this.state.bodyText} onChange={event => this.onBodyTextChange(event.target.value)}/>
-                            </Form.Field>
-                        </Form>
-
-                        <Button color="green" onClick={() => this.onSubmitButtonClicked()}>
-                            Submit
-                        </Button>
-                    </Card.Content>
-                </Card>
-            </div>
+                        <Button color="blue" className="btn"> Add </Button>
+                   </Form>
+                </Card.Content>
+            </Card>
+        </Grid.Column>
+    </Grid.Row>
+</Grid>
         )
     }
 }
--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
 import App from './App';
+import './app.css'
 
 ReactDOM.render(<App />, document.getElementById('root'));
