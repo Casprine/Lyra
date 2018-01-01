cc3427565b3cd0b52890caac4902fc410a1bfef2
Raaj Ahiable <raajable@gmail.com>
2018-01-01 03:01:13 +0000

    Set up logic for filling content form
    Signed-off-by: Raaj <raaja847@gmail.com>

--- dashboard/src/components/ContentForm.js
+++ dashboard/src/components/ContentForm.js
@@ -1,8 +1,32 @@
 import React, { Component } from 'react';
-import { Card, Form, Input } from 'semantic-ui-react';
+import { Card, Form, Input, Button } from 'semantic-ui-react';
 
 
 class ContentForm extends Component{
+
+    constructor(props){
+        super(props);
+        /* Created a new resources object because we can't mutate state directly. We mutate this object
+        before setting the state
+        */
+        this.resources = {};
+        this.state = { resources : {} }
+    }
+
+    componentWillMount(){
+        // Creating a resources form object based on model received from prop
+        Object.keys(this.props.resources).map(key => {
+            this.resources[key] = '';
+            console.log(this.resources);
+        })
+    }
+
+    onChangeForm(key, value){
+        this.resources[key] = value;
+        this.setState({ resources : this.resources }, () => {
+            console.log(this.state.resources);
+        });
+    }
 
     render(){
         if(this.props.resources === false){
@@ -18,12 +42,16 @@ class ContentForm extends Component{
                     <Form style={{ padding : '8px' }}>
                         {Object.keys(this.props.resources).map(key => {
                             return(
-                                <Form.Field>
+                                <Form.Field key={key}>
                                     <label>{key}</label>
-                                    <Input placeholder={this.props.resources[key]}/>
+                                    <Input
+                                        placeholder={this.props.resources[key]}
+                                        onChange={event => this.onChangeForm(key, event.target.value)}
+                                    />
                                 </Form.Field>
                             )
                         })}
+                        <Button color='green'>Submit</Button>
                     </Form>
                 </Card>
             )
