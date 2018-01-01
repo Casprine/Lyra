05da44550ba00edeadff21d7fb8218027c251544
Raaj <raaja847@gmail.com>
2018-01-01 04:07:03 +0000

    Set up dynamic port
    Signed-off-by: Raaj <raaja847@gmail.com>

--- server.js
+++ server.js
@@ -27,6 +27,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(routes);
 
-app.listen(5000, () => {
-    console.info('Server started')
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Server started on ${port}`);
 });
