1724252ffb7b07e10d801fb588b689ab305f69dd
Raaj <raaja847@gmail.com>
2018-01-01 03:37:54 +0000

    Enforced types in content form and also set up api request
    Signed-off-by: Raaj <raaja847@gmail.com>

--- dashboard/src/components/ContentForm.js
+++ dashboard/src/components/ContentForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Card, Form, Input, Button } from 'semantic-ui-react';
+import axios from 'axios';
 
 
 class ContentForm extends Component{
@@ -28,6 +29,18 @@ class ContentForm extends Component{
         });
     }
 
+    onSubmitClick(){
+        axios.post('/addContent', {
+            resources : this.state.resources
+        })
+            .then(response => {
+                console.log(response.data);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+
     render(){
         if(this.props.resources === false){
             return(
@@ -45,13 +58,14 @@ class ContentForm extends Component{
                                 <Form.Field key={key}>
                                     <label>{key}</label>
                                     <Input
+                                        type={this.props.resources[key]}
                                         placeholder={this.props.resources[key]}
                                         onChange={event => this.onChangeForm(key, event.target.value)}
                                     />
                                 </Form.Field>
                             )
                         })}
-                        <Button color='green'>Submit</Button>
+                        <Button color='green' onClick={ () => this.onSubmitClick() }>Submit</Button>
                     </Form>
                 </Card>
             )
