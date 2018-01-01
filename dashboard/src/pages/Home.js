635bccca672ae384cf1f85be66ec8aff5eb22158
Raaj <raaja847@gmail.com>
2018-01-01 02:19:01 +0000

    Set up react router links

--- dashboard/src/pages/Home.js
+++ dashboard/src/pages/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Button } from 'semantic-ui-react'
+import { Link } from 'react-router-dom';
 
 class Home extends Component{
     constructor(props){
@@ -13,12 +14,12 @@ class Home extends Component{
                     <img src="http://res.cloudinary.com/kre/image/upload/v1514318568/shot_seo_50_rqnkqc.png"/>
                 </div>
                 <div className="title"> Lyra </div>
-                <div className="about"> The headles CMS for babies <i class="em em-baby"></i></div> 
+                <div className="about"> The headless CMS for babies <span class="em em-baby"/></div>
 
 
                 <div className="clicks"> 
-                    <Button primary>Dashboard</Button>
-                    <Button color='olive'>Create Models</Button>
+                    <Link to='/addContent'><Button primary>Dashboard</Button></Link>
+                    <Link to='/config'><Button color='olive'>Create Models</Button></Link>
                 </div>
             </div>
 
